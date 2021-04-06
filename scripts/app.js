cb17d6e658dbd99c975e32620cae01f93b0f452d
Félix Dorn <github@felixdorn.fr>
2021-04-06 14:09:23 +0200

    :lipstick: responsiveness

--- scripts/app.js
+++ scripts/app.js
@@ -28,13 +28,13 @@ const loadLinks = (search) => {
             const link = links[name]
             const el = document.createElement('div')
 
-            el.innerHTML = `<a href="${link}" class="flex justify-between items-start sm:items-stretch rounded-lg p-4 transition ease-in-out duration-150 bg-gray-800 border-t border-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 focus:ring-offset-gray-800 focus:outline-none">
+            el.innerHTML = `<a href="${link}" class="flex flex-col sm:flex-row justify-between items-start sm:items-stretch rounded-lg p-4 transition ease-in-out duration-150 bg-gray-800 border-t border-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 focus:ring-offset-gray-800 focus:outline-none">
                 <div class="flex flex-col">
                     <p class="font-semibold select-none">${name}</p>
-                    <p class="text-gray-400 hover:underline text-xs sm:text-base">${link}</p>
+                    <p class="text-gray-400 hover:underline text-xs sm:text-base mt-1">${link}</p>
                 </div>
                 <button type="button"
-                        class="transition ease-in-out duration-150 bg-gradient-to-t from-purple-500 to-purple-400 px-6 py-2 rounded-lg font-semibold text-white hover:from-purple-400 hover:to-purple-500 focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ring-offset-gray-700 focus:outline-none">
+                        class="mt-4 sm:mt-0 w-full sm:w-auto transition ease-in-out duration-150 bg-gradient-to-t from-purple-600 to-purple-500 px-6 py-2 rounded-lg font-semibold text-white hover:from-purple-400 hover:to-purple-500 focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ring-offset-gray-700 focus:outline-none">
                     Continuer
                 </button>
             </a>`
